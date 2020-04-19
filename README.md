a78bc3b7cc44ffcba3b46e809c8589d93b4419e7
paulo-789 <jorhnepaulone@gmail.com>
2020-04-19 22:48:23 +0300

    updated read me

---
+++ README.md
@@ -0,0 +1,61 @@
+# TItle
+ 
+Caesar-Cipher
+
+ ## Author
+ Paul Onyango
+ A software developer.
+
+
+## Description
+ Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, 
+ is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. 
+ For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.[1]
+
+
+## Project set up
+ * Open the terminal in your device.
+ * git clone
+ * Open the project in Intellij IDEA
+ * Run the program 
+
+
+ 
+
+## Technologies used
+
+* Java
+
+
+## Behaviour Driven Development(BDD)
+When you run the app you are welcomed and the instructed to input 
+the text to be Encrypted and when you press enter you are asked to
+input the shift key which it should be in numbers.Then  you receive 
+your out put giving you the Encrypted Text plus the Decrypted text(the orginal text).
+
+
+ ### Contacts
+  Email:paulnyango021@gmail.com
+
+## Licence
+MIT License
+
+Copyright (c) [2020] [PAUL Onyango]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,9 +1,78 @@
 import java.util.Scanner;
 
-public class CaesarCipher{
+public class CaesarCipher {
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Caesar Cipher");
 
-    };
+  //using scanner to collect what aperson inputs
+        Scanner aScanner = new Scanner(System.in);
+        String plainText;
+        int shiftKey;
+        System.out.println("WElcome");
+        System.out.println("Please enter message to be encrypted: ");
+        plainText = aScanner.nextLine();
 
-};
+        System.out.println("Please enter shift key ");
+        shiftKey = aScanner.nextInt();
+        String encryptedText = EncryptText(plainText, shiftKey);
+
+        System.out.println("The Encrypted Text: " + encryptedText);
+        System.out.println("The Decrypted Text: "+DecryptText(encryptedText,shiftKey));
+    }
+    public static String EncryptText(String message, int shiftKey) {
+        //c(x) =  x + shiftKey Mod(%) 26;
+        final String ALPAHABETS = "abcdefghijklmnopqrstuvwxyz";
+        message = message.toLowerCase();
+
+        StringBuilder cyperText = new StringBuilder();
+
+        for(int counter = 0;counter < message.length();counter++) {
+            if(!Character.isLetter(message.charAt(counter))) {
+                cyperText.append(message.charAt(counter));
+            }
+            else {
+                int charPosition = ALPAHABETS.indexOf(message.charAt(counter));
+                int keyValue = (charPosition + shiftKey) % 26;
+
+                char cyperValue = ALPAHABETS.charAt(keyValue);
+
+                cyperText.append(cyperValue);
+            }
+        }
+
+        return cyperText.toString();
+    }
+    public static String DecryptText(String message, int shiftKey) {
+        //c(x) =  x - shiftKey Mod(%) 26; fomular
+        final String ALPAHABETS = "abcdefghijklmnopqrstuvwxyz";
+
+        message = message.toLowerCase();
+
+        StringBuilder plainText = new StringBuilder();
+
+        for(int counter = 0;counter < message.length();counter++) {
+
+            if(!Character.isLetter(message.charAt(counter))) {
+                plainText.append(message.charAt(counter));
+            }
+
+            else {
+                int charPosition = ALPAHABETS.indexOf(message.charAt(counter));
+                int keyValue = (charPosition - shiftKey) % 26;
+
+                if (keyValue < 0) {
+
+                    keyValue = ALPAHABETS.length() + keyValue;
+                }
+
+                char plainValue = ALPAHABETS.charAt(keyValue);
+
+                plainText.append(plainValue);
+            }
+        }
+
+        return plainText.toString();
+
+    }
+
+}
